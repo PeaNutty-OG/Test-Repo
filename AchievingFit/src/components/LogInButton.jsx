ec54782a5f8357d1f5087f97db3a2f42bafbf6a3
PeaNutty_OG <HockeyDude7922@gmail.com>
2025-05-07 14:02:27 -0500

    Added react-router-dom and LogIn Button

---
+++ AchievingFit/src/components/LogInButton.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Button from "@mui/joy/Button";
+
+export default function LogInButton() {
+  return (
+    <Button color="neutral" variant="soft">
+      Log In
+    </Button>
+  );
+}
---
+++ AchievingFit/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import TabBar from "../components/TabBar";
+import LogInButton from "../components/LogInButton";
+
+export default function Home() {
+  return (
+    <>
+      <LogInButton />
+      <TabBar />
+    </>
+  );
+}
